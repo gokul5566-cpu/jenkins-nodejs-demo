84a42c10d24da5f9767c699c732d643656670a62
Roddam Gokul <54104547+gokul5566-cpu@users.noreply.github.com>
2024-12-09 18:12:09 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting deployment..."
 # Copy files to the deploy directory (e.g., /var/www/html for a web server)
-sudo mkdir -p /var/www/html/jenkins-nodejs-demo
+mkdir -p /var/www/html/jenkins-nodejs-demo
 cp -r app/* /var/www/html/jenkins-nodejs-demo/
 echo "Deployment completed successfully."
